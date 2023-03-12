00545fa9a2a7b0a521ef3a85edd0f35a63552bb4
camilo <camiloacevedomoreno@gmail.com>
2023-03-11 21:23:57 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,11 @@ import {nanoid} from "nanoid"
 
 // Sync notes with localStorage - branch: notes-to-localStorage
 // Add note summary titles
-/**
-     * Challenge: Try to figure out a way to display only the 
-     * first line of note.body as the note summary in the
-     * sidebar.
-     * 
-     * Hint 1: note.body has "invisible" newline characters
-     * in the text every time there's a new line shown. E.g.
-     * the text in Note 1 is:
-     * "# Note summary\n\nBeginning of the note"
-     * 
-     * Hint 2: See if you can split the string into an array
-     * using the "\n" newline character as the divider
-     */
 // TODO Move modified notes to the top of the list
+    /**
+     * Challenge: When the user edits a note, reposition
+     * it in the list of notes to the top of the list
+     */
 // TODO Delete notes
 
 
